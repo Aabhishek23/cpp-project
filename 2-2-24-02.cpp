27dd44166a4e70236bd82f80d8a0b67f580e5556
Aabhishek23 <95581248+Aabhishek23@users.noreply.github.com>
2024-02-02 15:25:15 +0530

    2-2-24

---
+++ 2-2-24-02.cpp
@@ -0,0 +1,114 @@
+#include <bits/stdc++.h>
+
+
+using namespace std;
+
+
+int main(){
+
+
+//int n=20;
+
+/*
+for(int i=0;pow(2,i)<=n;++i){
+
+    cout<<pow(2,i) << " " ;
+
+}
+*/
+
+
+/*
+for(int i=n;i>=1;i=i/2){
+
+    cout<<i << " " ;
+
+}
+*/
+
+
+/*
+for(int i=n;i>=2;--i){
+
+   if(i%2==0)
+   {
+       cout<<i<<' ';
+   }
+}
+*/
+
+/*
+int x=20;
+
+while(x>=2){
+
+    cout<< x <<" " ;
+
+    x=x-2;
+
+}
+*/
+
+
+/*
+int n=6;
+int i=1;
+
+while(i<=n){
+
+cout<< i<<" " ;
+i++;
+}
+
+*/
+
+/*
+int n=6;
+int i=n;
+
+while(i>=1){
+
+cout<< i<<" " ;
+i--;
+}
+
+
+*/
+
+
+/*
+int n=20;
+int i=0;
+
+while(pow(2,i)<=n){
+
+cout<<pow(2,i) << " " ;
+
+i++;
+}
+
+*/
+
+
+int n=16;
+int i=n;
+
+while(i>=1){
+
+cout<<i<< " " ;
+
+i=(i/2);
+
+}
+
+
+
+
+return 0;
+
+
+}
+
+
+
+
---
+++ 2-2-24.c
@@ -0,0 +1,15 @@
+#include <bits/stdx++.h>
+
+
+using namespace std;
+
+
+int main(){
+int n=6;
+
+
+for(int i=n;i>=1;--i){
+    cout << i<< ' ';
+}
+
+}
