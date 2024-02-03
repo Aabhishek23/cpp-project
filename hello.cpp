c7c43340fcd3757523cf64854d770c99a4f2cc12
Aabhishek23 <95581248+Aabhishek23@users.noreply.github.com>
2024-02-03 13:53:06 +0530

    03-02-24

---
+++ hello.cpp
@@ -0,0 +1,54 @@
+#include<bits/stdc++.h>
+
+int main(){
+
+using namespace std;
+
+  /*  int n=3,m=4;
+
+    for(int i=1;i<=n;++i){
+
+    cout<<i<<": ";
+        for(int j=1;j<=m;++j){
+
+        cout<<j<<" ";
+
+    }
+    cout <<endl;
+
+    }
+
+
+        int n=3,m=4;
+
+    for(int i=1;i<=n;++i){
+
+    cout<<i<<": ";
+        for(int j=1;j<=m;++j){
+
+        cout<<j<<" ";
+
+    }
+    cout <<endl;
+
+    }
+
+    */
+
+
+    int n=3,m=4;
+
+    for(int i=1;i<=n;++i){
+
+    //cout<<i<<": ";
+        for(int j=1;j<=m;++j){
+
+        cout<<i<<" ";
+
+    }
+    cout <<endl;
+
+    }
+
+    return 0;
+}
