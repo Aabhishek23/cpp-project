8b9b15705b9d169c1749f6b7a44e564bb589fe94
Aabhishek23 <95581248+Aabhishek23@users.noreply.github.com>
2024-02-10 14:04:11 +0530

    10-02-24(1)

--- Untitled1.cpp
+++ Untitled1.cpp
@@ -1,22 +1,12 @@
 #include<iostream>
+
+
 using namespace std;
+int main(){
 
-int main()
-{
-
-    int abalibleSeat=10;
-    int requestedSeats;
-    cout<<"Welocome therter ! Ther are "<<abalibleSeat<<"seat availible"<<endl;
-    cout<<"How many seat would  you like to reserve?";
-    cin>>abalibleSeat;
-    while(requestedSeats>>abalibleSeat){
-        cout<<"Sorry we have"<<abalibleSeat<<"seat availible"<<endl;
-        cout<<"Please smaller number of seats";
-        cin>>requestedSeats;
-    }
-    cout<<"Reservation successful! You have reserved"<<requestedSeats<<"Seats ."<<endl;
-    abalibleSeat=requestedSeats;
-    cout<<"There are now"<<abalibleSeat<<"seats remining"<<endl;
-    return 0;
-
+int a=2;
+double b=2.5 53989439043805885;
+float d=2.553989439043805885;
+cout<<b<<endl;
+cout<<d;
 }
