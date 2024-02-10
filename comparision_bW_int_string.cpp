e8cbde8b5bdd1d36b580d313140738a342757e45
Aabhishek23 <95581248+Aabhishek23@users.noreply.github.com>
2024-02-10 15:19:50 +0530

    Add files via upload

---
+++ comparision_bW_int_string.cpp
@@ -0,0 +1,15 @@
+
+#include<iostream>
+
+using namespace std;
+int main(){
+
+    string number="1";
+    int num=1;
+
+    cout<<number+number<<endl;
+    cout<<num+num<<endl;
+
+    return 0;
+}
+
