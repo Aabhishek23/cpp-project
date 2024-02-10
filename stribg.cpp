c7fdfcc47b1432734f6438b84c45b6701ce3de76
Aabhishek23 <95581248+Aabhishek23@users.noreply.github.com>
2024-02-10 14:34:20 +0530

    2-10-24-2:34

---
+++ stribg.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+
+using namespace std;
+int main(){
+    //cherecters
+    //for storing gender
+    //for stornig char value:
+    //for storing gender
+    string name,age,gender;
+
+
+    cout<<"ENTER YOUR NAME :";
+    cin>>name;
+    cout<<"ENTER YOUR AGE :";
+    cin>>age;
+    cout<<"ENTER YOUR GENDER :";
+    cin>>gender;
+
+
+    cout << "Your name is "<<name<<endl;
+    cout << "Your age is "<<age<<endl;
+    cout << "Your gen is "<<gender<<endl;
+
+    return 0;
+}
+
