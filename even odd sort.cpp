b074a8ab617209fd17196dcd9200b30b620ecdb4
Aabhishek23 <95581248+Aabhishek23@users.noreply.github.com>
2024-02-18 23:44:27 +0530

    even odd sort update 1.0

--- even odd sort.cpp
+++ even odd sort.cpp
@@ -3,27 +3,34 @@ using namespace std;
 
 int main()
 {
-    int num1[6]={7,9,9,9,4,7};
-    int num2[6]={NULL};
-    int num3[6]={NULL};
+    int num1[6]= {7,9,9,9,4,7};
+    int num2[6]= {NULL};
+    int num3[6]= {NULL};
 
 
-    for(int i=0;i<6;i++){
+    for(int i=0; i<6; i++)
+    {
 
-        if(num1[i]%2==0){
+        if(num1[i]%2==0)
+        {
             num2[i]=num1[i];
         }
 
-        else{
+        else
+        {
             num3[i]=num1[i];
         }
     }
 
 
 
-    for(int j=0;j<6;j++){
+    for(int j=0; j<6; j++)
+    {
 
-        if(num2[j]!=NULL){cout<<num2[j]<<" ";}
+        if(num2[j]!=NULL)
+        {
+            cout<<num2[j]<<" ";
+        }
 
     }
 
@@ -32,10 +39,11 @@ int main()
 
 
 
-    for(int k=0;k<6;k++){
+    for(int k=0; k<6; k++)
+    {
         if(num3[k]!=NULL)cout<<num3[k]<<" ";
     }
 
-return 0;
+    return 0;
 
 }
